8e4e6898408e1d21a09ceae86d129846a8d61624
BanulaRPBS <bsbanula@gmail.com>
2025-04-27 09:41:44 +0530

    Useredit controller
    Useredit controller initialisation

---
+++ backend/src/Controllers/UserEditController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using StudentApp.Data;
+using StudentApp.Models;
+
+namespace StudentApp.Controllers
+{
+    [Route("api/user-edit")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IPasswordHasher<User> _passwordHasher;
+
+        public UsersController(ApplicationDbContext context, IPasswordHasher<User> passwordHasher)
+        {
+            _context = context;
+            _passwordHasher = passwordHasher;
+        }
+
+        /// <summary>
+        /// Gets all users.
+        /// </summary>
+        
+         // DTOs (Data Transfer Objects) for user creation and updating
+    public class CreateUserModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+
+    public class UpdateUserModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
