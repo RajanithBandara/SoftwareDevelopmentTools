c2f67a2c83ecd39ea59baa86eb4caa6e49560804
NPVinodya <prabanivinodya@gmail.com>
2025-04-27 16:14:57 +0530

    Add SettingsController with basic API route setup.🚀

---
+++ backend/src/Controllers/SettingsController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentApp.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using StudentApp.Models;
+
+namespace StudentApp.Controllers
+{
+    [Route("api/settings")]
+    [ApiController]
+    public class SettingsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SettingsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
